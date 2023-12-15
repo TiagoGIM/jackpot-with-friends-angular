c77b42b1780f11bb36cacbc9297d10a74ecc0e17
tiagoGIM <herique.sa@gmail.com>
2023-12-15 14:09:36 -0300

    feat:  aply number picker css

--- src/app/pages/create-ticket/create-ticket.component.ts
+++ src/app/pages/create-ticket/create-ticket.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'jwf-create-ticket',
   templateUrl: './create-ticket.component.html',
-  styleUrls: ['./create-ticket.component.css']
+  styleUrls: ['./create-ticket.component.css'],
 })
 export class CreateTicketComponent {
+  selectedNumbers: number[] = Array(6).fill(null);
+  allNumbers: number[] = [];
+  ticketLength: number = 6;
+  editingIndex = 0;
 
+  constructor() {
+    this.initializeNumbers();
+  }
+
+  private initializeNumbers() {
+    const totalNumbers = 60;
+    this.allNumbers = Array.from(
+      { length: totalNumbers },
+      (_, index) => index + 1
+    );
+  }
+
+  isSelected(num: number): boolean {
+    return this.selectedNumbers.includes(num);
+  }
 }
