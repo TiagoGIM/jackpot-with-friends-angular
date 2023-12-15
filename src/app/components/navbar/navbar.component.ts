e1d149f0b9dd158b6515bd49c7f1a098c05c8f5d
tiagoGIM <herique.sa@gmail.com>
2023-12-15 13:35:55 -0300

    feat: simple navbar

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jwf-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+
+}
