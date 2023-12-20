8f70257daca10a041b69d4c3c76d383838de61dc
tiagoGIM <herique.sa@gmail.com>
2023-12-19 21:09:22 -0300

    feat: redirect to ticket list

--- src/app/store/tickets/ticket.effects.ts
+++ src/app/store/tickets/ticket.effects.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { createEffect, ofType, Actions } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { map, catchError, of, switchMap } from 'rxjs';
+import { map, catchError, of, switchMap, exhaustMap, tap } from 'rxjs';
 import * as TicketsActions from './ticket.actions';
 import { TicketsService } from '../../services/ticket.service';
 import { ErrorHttp } from '../login/login.effects';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class TicketsEffects {
@@ -21,26 +22,30 @@ export class TicketsEffects {
       )
     )
   );
-
   editTicket$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(TicketsActions.editActiveTicket),
-      switchMap((ticket) =>
-        this.ticketsService
-          .updateTicket(ticket)
-          .pipe(
-            map((updatedTicket) =>
-              TicketsActions.editActiveTicketSuccess({ updatedTicket }))
+  this.actions$.pipe(
+    ofType(TicketsActions.editActiveTicket),
+    switchMap((ticket) =>
+      this.ticketsService
+        .updateTicket(ticket)
+        .pipe(
+          map((updatedTicket) =>
+            TicketsActions.editActiveTicketSuccess({ updatedTicket })
+          ),
+          catchError((error: ErrorHttp) =>
+            of(TicketsActions.editActiveTicketFailure({ error: error.statusText }))
           )
-      ),
-      catchError((error: ErrorHttp) =>
-        of(TicketsActions.editActiveTicketFailure({ error : error.statusText }))
-      )
-    )
-  );
+        )
+    ),
+    tap(() => {
+      this.router.navigate(['/ticket-list']);
+    })
+  )
+);
 
   constructor(
     private actions$: Actions,
-    private ticketsService: TicketsService
+    private ticketsService: TicketsService,
+    private router : Router
   ) {}
 }
