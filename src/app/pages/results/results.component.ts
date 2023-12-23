d6738d8e86682767acfcdec79f22745229435469
tiagoGIM <herique.sa@gmail.com>
2023-12-23 16:29:45 -0300

    chore: avoid install conflitcts

--- src/app/pages/results/results.component.ts
+++ src/app/pages/results/results.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
+import { Observable, map, tap } from 'rxjs';
 import { TicketsService } from 'src/app/services/ticket.service';
 
+interface OddsItem {
+  number : number,
+  quantity: number
+}
 @Component({
   selector: 'jwf-results',
   templateUrl: './results.component.html',
@@ -10,5 +15,14 @@ export class ResultsComponent {
  constructor(
   private ticketService : TicketsService
  ){}
- odds$ = this.ticketService.loadOdds()
+ showAll: boolean = true
+ odds$ : Observable<OddsItem[]>= this.ticketService.loadOdds().pipe(
+  map((numbers) =>
+  numbers.sort((a: OddsItem, b: OddsItem) => b.quantity - a.quantity).slice(0,  10)
+)
+  
+ )
+
+
+
 }
